fc866751535dae66ef4eab0fbf8a4bbaf60939d7
hong Kil Dong <sion9199@gmail.com>
2024-12-11 12:39:25 +0900

    쿠키 연습

--- day4/cookie/CookieGetServlet.java
+++ day4/cookie/CookieGetServlet.java
@@ -1,13 +1,14 @@
 package cookie;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.io.PrintWriter;
 
 @WebServlet("/cookieget")
 public class CookieGetServlet extends HttpServlet {
@@ -17,8 +18,13 @@ public class CookieGetServlet extends HttpServlet {
 		//1. 쿠키 객체를 가져온다
 		Cookie[] cookies= request.getCookies();
 		//2. 쿠키 정보를 출력한다
+		if(cookies!=null) {
 		for (int i = 0; i < cookies.length; i++) {
 			result+= cookies[i].getName()+" : "+ cookies[i].getValue()+"<br>";
+		}
+		}
+		else {
+			result+="<h3>저장된 쿠키 없음</h3>";
 		}
 		response.setContentType("text/html;chatset=utf-8");
 		PrintWriter o = response.getWriter();
--- day4/cookie/CookieRemoveServlet.java
+++ day4/cookie/CookieRemoveServlet.java
@@ -2,10 +2,12 @@ package cookie;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 
 @WebServlet("/cookieremove")
@@ -14,7 +16,28 @@ public class CookieRemoveServlet extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		//특정 쿠키 1개삭제
+		/*Cookie cookie = new Cookie("name", "");
+		cookie.setMaxAge(0);
+		response.addCookie(cookie);
+		
+		String result = "<h3>쿠키가 삭제되었습니다.</h3>";
+		result +="<a href=cookieget> 쿠기삭제확인</a>";
+		response.setContentType("text/html;chatset=utf-8");
+		PrintWriter o = response.getWriter();
+		o.println(result);*/
+		
+		
 		//모든 쿠키 삭제
+		Cookie[] cookies=request.getCookies();
+		for (int i = 0; i < cookies.length; i++) {
+			cookies[i].setMaxAge(0);
+			response.addCookie(cookies[i]);
+		}
+		String result = "<h3>모든 쿠키가 삭제되었습니다.</h3>";
+		result +="<a href=cookieget> 쿠기삭제확인</a>";
+		response.setContentType("text/html;chatset=utf-8");
+		PrintWriter o = response.getWriter();
+		o.println(result);
 	}
 
 }
